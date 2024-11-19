57c8d368be03023ea617626fe6fdb995f5cd4850
happy <happyraj9939@gmail.com>
2024-11-19 22:45:16 +0530

    ownBook page

--- src/app/ownBook/page.tsx
+++ src/app/ownBook/page.tsx
@@ -1,40 +1,60 @@
-import { getDataFromToken } from "@/helpers/getDataFromToken";
-import dbConnect from "@/lib/dbConnect";
-import Book from "@/model/books";
-import { NextRequest, NextResponse } from "next/server";
+"use client";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import ProductCard from "@/components/productBookCard";
 
-export async function GET(request: NextRequest) {
-  await dbConnect(); 
 
-  try {
-    const id = await getDataFromToken(request);
+interface Product {
+  _id: string;
+  image: "";
+  boardName: "";
+  className: "";
+  subjectName: "";
+  price: "";
+  edition: "";
+  slug:"",
+}
 
-    // Fetch books for the user
-    const product = await Book.aggregate([
-      {
-        $match: {
-          userId: id
-        }
+interface APIResponse {
+  message: Product[];
+}
+
+export default function Home() {
+  const [products, setProducts] = useState<Product[]>([]);
+  
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await axios.get<APIResponse>("/api/ownBook");
+        console.log(res.data);
+        setProducts(res.data.message);
+      } catch (error: any) {
+        console.log(error.message);
       }
-    ]).exec();
+    };
+    fetchProducts();
+  }, []);
 
-    if (!product || product.length === 0) {
-      return NextResponse.json({
-        success: false,
-        message: "Book not found"
-      }, { status: 404 });
-    }
+  
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between bg-gradient-to-r from-blue-50 via-gray-100 to-gray-200 p-8 sm:p-8">
 
-    return NextResponse.json({
-      success: true,
-      message: product
-    }, { status: 200 });
-
-  } catch (error: any) {
-    console.log("Get Book error", { error });
-    return NextResponse.json({
-      success: false,
-      message: error.message
-    }, { status: 500 });
-  }
+<div className="flex flex-col gap-10 p-8 px-3 max-w-6xl mx-auto">
+        <div className="max-w-6xl mx-auto p-3 flex flex-col gap-10 py-7">
+          {products && products.length > 0 ? (
+            <div className="flex flex-col gap-6">
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+                {products.map((product) => (
+                  <ProductCard key={product._id} product={product} />
+                ))}
+              </div>
+            </div>
+          ) : (
+            <p className="text-center text-lg text-gray-500">No products available</p>
+          )}
+        </div>
+      </div>    
+    </main>
+  );
 }
